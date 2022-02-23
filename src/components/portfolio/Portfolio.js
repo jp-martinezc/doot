1cd5569e96958ec1bb6acf21b9f39845f88ec74b
unknown <jp.martinezc@uniandes.edu.co>
2022-02-23 15:29:39 -0500

    funcionan los skills para pantallas grandes

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./portfolio.scss"
 import javaImg from '../../images/java_logo_640.jpg';
 import PortfolioList from "../portfolioList/PortfolioList";
-import {featuredPortfolio} from '../../data';
+import {featuredPortfolio,
+     webPortfolio,
+      mobilePortfolio,
+       designPortfolio,
+        dataPortfolio } from '../../data';
 
 
 export default function Portfolio( {queMenu, setQueMenu} ) {
@@ -10,6 +14,7 @@ export default function Portfolio( {queMenu, setQueMenu} ) {
     const [movido, setMovido] = useState("nomovido");
     console.log(movido);
     const [selected, setSelected] = useState("featured");
+    const [data, setData] = useState([]);
 
     const list = [
         {
@@ -34,61 +39,60 @@ export default function Portfolio( {queMenu, setQueMenu} ) {
         },
     ];
 
+    useEffect(()=> {
+
+        switch(selected){
+            case "featured":
+                setData(featuredPortfolio);
+                break;
+            case "web":
+                setData(webPortfolio);
+                break; 
+            case "mobile":
+                setData(mobilePortfolio);
+                break; 
+            case "design":
+                setData(designPortfolio);
+                break;  
+            case "data":
+                setData(dataPortfolio);
+                break;
+        }
+
+    },[selected]);
+
     return(
         <div className={"portfolio " + movido} id="portfolio" onMouseOver={()=> a()}>
             
 
             <div className="cartuchis"> 
-            <h1>Skills</h1>
+    <div>
+    <h1>Skills</h1>
+    </div>
+          
             <ul>
                 {list.map((item) => (
                     <PortfolioList title={item.title} active={selected === item.id} setSelected={setSelected} id={item.id}/>
                 ))}
             </ul>
             <div className="container">
-            <div className="item">
-                <img className="imagenItem" src={javaImg} alt="">
+
+            {data.map((d) => (
+                 <div className="item">
+                 <img className="imagenItem" src={d.img} alt="">
+                 
+                     
+                 </img>
+                 <h3>{d.title}</h3>
+             </div>
                 
-                    
-                </img>
-                <h3>app</h3>
-            </div>
-            <div className="item">
-                <img className="imagenItem" src={javaImg} alt="">
+            ))}
+           
+            
                 
-                    
-                </img>
-                <h3>app</h3>
+          
             </div>
-            <div className="item">
-                <img className="imagenItem" src={javaImg} alt="">
-                
-                    
-                </img>
-                <h3>app</h3>
             </div>
-            <div className="item">
-                <img className="imagenItem" src={javaImg} alt="">
-                
-                    
-                </img>
-                <h3>app</h3>
-            </div>
-            <div className="item">
-                <img className="imagenItem" src={javaImg} alt="">
-                
-                    
-                </img>
-                <h3>app</h3>
-            </div>
-            <div className="item">
-                <img className="imagenItem" src={javaImg} alt="">
-                
-                    
-                </img>
-                <h3>app</h3>
-            </div>
-            </div></div>
             <div className="barragonita"> </div>
            
             
--- src/data.js
+++ src/data.js
@@ -6,6 +6,19 @@ import r from './images/R.jpg';
 import dart from './images/dart.png';
 import react from './images/react.png';
 import angular from './images/angular.png';
+import express from './images/express.png';
+import node from './images/node.png';
+import bootstrap from './images/bootstrap.png';
+import flutter from './images/flutter.jpg';
+import photoshop from './images/photoshop.jpg';
+import fusion from './images/fusion.jpg';
+import procreate from './images/procreate.png';
+import osi from './images/osi.png';
+import pandas from './images/pandas.png';
+import knime from './images/knime.png';
+import powerbi from './images/powerbi.png';
+
+
 
 
 export const featuredPortfolio = [
@@ -41,7 +54,7 @@ export const featuredPortfolio = [
     },
 ];
 
-export const webPorftfolio = [
+export const webPortfolio = [
     {
         id: 1,
         title: "React",
@@ -52,5 +65,73 @@ export const webPorftfolio = [
         title: "Angular",
         img: angular,
     },
+    {
+        id: 3,
+        title: "Node",
+        img: node,
+    },
+    {
+        id: 4,
+        title: "Bootstrap",
+        img: bootstrap,
+    },
+    {
+        id: 5,
+        title: "Express",
+        img: express,
+    },
 
+];
+
+
+export const mobilePortfolio =  [
+    {
+        id: 1,
+        title: "Flutter",
+        img: flutter,
+    },
+
+];
+
+export const designPortfolio = [
+
+    {
+        id: 1,
+        title: "Adobe Photoshop",
+        img: photoshop,
+    },
+    {
+        id: 2,
+        title: "CAD Fusion 360",
+        img: fusion,
+    },
+    {
+        id: 3,
+        title: "Procreate",
+        img: procreate,
+    },
+
+];
+
+export const dataPortfolio = [
+    {
+        id: 1,
+        title: "Power BI",
+        img: powerbi,
+    },
+    {
+        id: 2,
+        title: "OSI Soft",
+        img: osi,
+    },
+    {
+        id: 3,
+        title: "Knime",
+        img: knime,
+    },
+    {
+        id: 4,
+        title: "Pandas",
+        img: pandas,
+    },
 ];
